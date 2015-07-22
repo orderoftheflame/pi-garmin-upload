46c626c11619390770515428cdc67b465521c173
Ben Mitchell <bmitchell@intelligentretail.co.uk>
2015-07-22 09:15:16 +0100

    Created basic Maven Java project

---
+++ src/main/java/co/ootf/garmin/GarminUploadApp.java
@@ -0,0 +1,16 @@
+package co.ootf.garmin;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class GarminUploadApp {
+
+    private static final Log LOG = LogFactory.getLog(GarminUploadApp.class);
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
