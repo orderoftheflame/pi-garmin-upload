99f6dfca59847d295fdbc089e060ef5260805d8c
Ben Mitchell <bmitchell@intelligentretail.co.uk>
2018-04-04 12:35:44 +0100

    Fixed typo

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,4 +4,4 @@ FOLDER=$1;
 CONFIG=$2;
 sudo mount $FOLDER;
 sleep 5;
-sudo java -jar /home/pi/pi-garmin-upload/release/pi-garmin-upload.jar $FOLDER /home/pi/$FOLDER.properties;
+sudo java -jar /home/pi/pi-garmin-upload/release/pi-garmin-upload.jar $FOLDER /home/pi/$CONFIG.properties;
